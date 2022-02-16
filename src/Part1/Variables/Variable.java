4d6d2511164e7a4e8ba20fb435094221fa27facf
maxlestage <maxlestage@icloud.com>
2022-02-16 14:58:50 +0100

    Variables 🔥

---
+++ src/Part1/Variables/Variable.java
@@ -0,0 +1,16 @@
+package Part1.Variables;
+
+public class Variable {
+    public static void main(String[] args) {
+        String text = "Contains Text";
+        int integer = 1234567890;
+        double pi = 3.14;
+        boolean trully = true;
+
+        // Print :
+        System.out.println("Text variable: " + text);
+        System.out.println("Integer variable: " + integer);
+        System.out.println("Floating-point variable: " + pi);
+        System.out.println("Boolean: " + trully);
+    }
+}
