ee51175153be5adb0102e9be44facfcfce6d0b01
maxlestage <maxlestage@icloud.com>
2022-02-17 15:55:04 +0100

    Variables ✅

---
+++ src/Part1/Variables/Doubles.java
@@ -0,0 +1,16 @@
+package Part1.Variables;
+
+import java.util.Scanner;
+
+public class Doubles {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a number:");
+        double value = Double.valueOf(scanner.nextLine());
+        System.out.println("You wrote " + value);
+        scanner.close();
+
+    }
+
+}
---
+++ src/Part1/Variables/ExtraSpace.java
@@ -0,0 +1,13 @@
+package Part1.Variables;
+
+import java.util.Scanner;
+
+public class ExtraSpace {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write a value ");
+        int data = Integer.valueOf(scanner.nextLine());
+        System.out.println("You gave the number " + data);
+        scanner.close();
+    }
+}
